245dd788bc0829576ad92c25b1dc46e0410ae3eb
Durgesh Pratap Singh <durgesh120103@gmail.com>
2025-02-13 03:03:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React , {Component} from "react"
+import Navbar from "./components/Navbar"
+
+const App = () => {
+  return (
+    <div className='px-4 md:px-8 lg:px-16 xl:px-32'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
